41f0d0c023b7cf1048abf12a98009a6a35420d2d
Lalit Tanwar <lalit.tanwar07@gmail.com>
2014-12-04 23:22:33 +0530

    updated modules

--- resources/persontest.js
+++ resources/persontest.js
@@ -1,13 +1,33 @@
 utils.define("persontest",function(persontest,_test_){
 	
+	/*
+	 * Import Dependencies
+	 */
+	utils.require("person","person_form");
+	
+	/*
+	 * Get Local references to modules
+	 */
 	var person = utils.module("person");
 	var person_form = utils.module("person_form");
 	
 	persontest._ready_ = function(){
 		
-		this.person = person.instance().init($("#person_module"));
+		/* 
+		 * create instance of each module
+		 */
+		this.personInstance = person.instance(); 
+		this.personFormInstance = person_form.instance();
 		
-		this.person_from = person_form.instance().init($("#person_form_module"));
+		/*
+		 * Because these are instances of modules exteded from 'utils.viewmodel'
+		 * each  has init function to initialize it in DOM;
+		 */ 
+		this.personInstance.init({
+			id : "#person_module"
+		});
+		this.personFormInstance.init($("#person_form_module"));
+	
 	};
 	
 });
